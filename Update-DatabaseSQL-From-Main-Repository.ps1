4db0ec97c3e5978cc511326d068ba24930b7a2b4
Stefan Hoffmann <stho32github@gmail.com>
2020-02-01 17:52:00 +0100

    add script to download database scripts from the main repository

---
+++ Update-DatabaseSQL-From-Main-Repository.ps1
@@ -0,0 +1,14 @@
+﻿<#
+    The main repository of MicroCloud is https://github.com/stho32/MicroCloud .
+    And this main repository essentially owns the database scripts. 
+
+    Since we need copies of the database for development of the apis, we need those scripts 
+    in here, too.
+
+    This script downloads the latest version of the sql scripts from the main repository into this
+    one, so both are in sync.
+#>
+Set-Location $PSScriptRoot
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/stho32/MicroCloud/master/Database/database.sql" -OutFile "$PSScriptRoot\Database\Database.sql"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/stho32/MicroCloud/master/Database/example-database-entries.sql" -OutFile "$PSScriptRoot\Database\example-database-entries.sql"
